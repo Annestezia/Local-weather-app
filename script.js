a7098f4fcd023dc0a0f2f57ff49ccae81fa0aacc
Annestezia <cast.wheel@gmail.com>
2020-02-06 02:26:47 +1000

    remake of the app with browser geolocation

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
-$(document).ready(function(){
-  //get location
-  const geoIP="https://api.ipdata.co/?api-key=test"
-  // freeGeoIP="https://freegeoip.net/json/";
-  $.getJSON(geoIP, function(ipData){
-  let {latitude, longitude,location,city,country_code}=ipData;
-    location= `${city}, ${country_code}`;
-  const darkSkyAPI=`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b1af518e5e126c8ce8367a1f38dc60fc/${latitude},${longitude}`;
-     //get forecast for  location    
-   $.getJSON(darkSkyAPI, function(weatherData){
-    const {summary,temperature,icon}=weatherData.currently;
-    $('#location').html(location);
-    $('#weatherCondition').html(summary);
-     //get and switch celsius&farengheit
-    var tempF = Math.floor(temperature);
-    var tempC = Math.floor((tempF-32)*5/9);
-    $('#temperature').html( tempC +' &deg;C') ;
-    $('#fahrenheit').on('click', function(){
-     $('#fahrenheit').parent().addClass('active');
-     $('#celsius').parent().removeClass('active');
-     $('#temperature').html( tempF +' &deg;F') ;
-   });
-  $('#celsius').on('click', function(){
-     $('#celsius').parent().addClass('active');
-     $('#fahrenheit').parent().removeClass('active');
-     $('#temperature').html( tempC +' &deg;C') ;
-   });
-     //all about icons
-     //Dark Sky says  thats all so far
-        let  iconClass;
-        switch(icon){
-          case 'clear-day':{ iconClass = 'wi wi-day-sunny'; break;}
-          case 'clear-night':{ iconClass = 'wi wi-night-clear'; break;}
-          case 'rain':{ iconClass = 'wi wi-rain'; break;}
-          case 'snow':{ iconClass = 'wi wi-snow'; break;}
-          case 'sleet':{ iconClass = 'wi wi-sleet'; break;}
-          case 'cloudy':{ iconClass = 'wi wi-cloudy'; break;}
-          case 'wind':{ iconClass = 'wi wi-windy'; break;}
-          case 'fog':{ iconClass = 'wi wi-fog'; break;}
-          case 'partly-cloudy-day':{ iconClass = 'wi wi-day-cloudy'; break;}
-          case 'partly-cloudy-night':{ iconClass = 'wi wi-night-partly-cloudy'; break;}
-          default:{iconClass = 'wi wi-na'; break;}
-                   }        
-        $('#weatherIcon i').addClass(iconClass);     
-    });    
-  });
-});
+(()=>{
+  let lat,lon, queryLink;
+  const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+  const darkskyEndpoint = 'https://api.darksky.net/forecast/';
+  const key ="b1af518e5e126c8ce8367a1f38dc60fc";
+
+  // getting lon lat
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(success);
+  }
+  //success callback for getCurrentPosition
+  function success(position){      
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    queryLink=`${corsAnywhere}${darkskyEndpoint}${key}/${lat},${lon}`;
+    //get weather data
+    $.getJSON(queryLink,json=>{
+      showForecast(json);
+    });
+  };
+  // Get class to display icon
+  const getIconClass=(icon)=>{
+    switch(icon){
+      case 'clear-day':{ return 'wi wi-day-sunny'; break;}
+      case 'clear-night':{ return 'wi wi-night-clear'; break;}
+      case 'rain':{ return 'wi wi-rain'; break;}
+      case 'snow':{ return 'wi wi-snow'; break;}
+      case 'sleet':{ return 'wi wi-sleet'; break;}
+      case 'cloudy':{ return 'wi wi-cloudy'; break;}
+      case 'wind':{ return 'wi wi-windy'; break;}
+      case 'fog':{return 'wi wi-fog'; break;}
+      case 'partly-cloudy-day':{ return 'wi wi-day-cloudy'; break;}
+      case 'partly-cloudy-night':{ return 'wi wi-night-partly-cloudy'; break;}
+      default:{return 'wi wi-na'; break;}
+    } 
+  };
+  //display data in app
+  const showForecast=(json)=>{
+    const {timezone}=json;
+    const {summary,icon,temperature}=json.currently;
+    const tempF = Math.floor(temperature);
+    const tempC = Math.floor((tempF-32)*5/9);
+    const iconClass=getIconClass(icon);
+    $('#location').text(timezone);
+    $('#weatherIcon').addClass(iconClass);
+    $('#weatherCondition').text(summary);
+    $('#temperature').text(`${tempC} \u2103`);     
+    convertFC(tempF, tempC);
+   };
+  const convertFC=(tempF, tempC) =>{
+    $('#fahrenheit').on('click', function () {
+      $('#fahrenheit').parent().addClass('active');
+      $('#celsius').parent().removeClass('active');
+      $('#temperature').text(`${tempF} \u2109`);
+    });
+    $('#celsius').on('click', function () {
+      $('#celsius').parent().addClass('active');
+      $('#fahrenheit').parent().removeClass('active');
+      $('#temperature').html(`${tempC} \u2103`);
+    });
+  }   
+})();
